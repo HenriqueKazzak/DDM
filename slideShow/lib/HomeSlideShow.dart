7ba508f9ce89144ee745aa0bef44383afc529540
hkarmier <henriquemendesk@gmail.com>
2022-12-08 00:42:41 -0300

    add new features

--- slideShow/lib/HomeSlideShow.dart
+++ slideShow/lib/HomeSlideShow.dart
@@ -9,19 +9,22 @@ class HomeSlideShow extends StatefulWidget {
 }
 
 class _HomeSlideShowState extends State<HomeSlideShow> {
-  List<Image> imageList = [Image.asset("lib/images/IMG_2857.jpg"),Image.asset("lib/images/IMG_2876.jpg"),Image.asset("lib/images/IMG_2925.jpg")];
+  List<Image> imageList = [Image.network("https://i.seadn.io/gae/2hDpuTi-0AMKvoZJGd-yKWvK4tKdQr_kLIpB_qSeMau2TNGCNidAosMEvrEXFO9G6tmlFlPQplpwiqirgrIPWnCKMvElaYgI-HiVvXc?auto=format&w=1000",width: 400),Image.network("https://media.moddb.com/images/members/5/4550/4549205/dog.jpg",width: 400),Image.network("http://images7.memedroid.com/images/UPLOADED894/5f0502441774c.jpeg",width: 400)];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Generico"),
       ),
-      body: ImageSlideshow(
+      body:Center(
+      child:ImageSlideshow(
+        width: 400,
+        isLoop: true,
         children: [
           for(var item in imageList)
             item
         ],
-      ),
+      )),
     );
   }
 }
